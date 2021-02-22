d26f46ebec3658486c7133777b65f76039a8458f
mhoobler <hoobler.michael@gmail.com>
2021-02-22 13:41:58 -0500

    added search bar, visual bug

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,24 +3,39 @@ import axios from 'axios';
 
 import ItemContainer from '../components/ItemContainer';
 
+import './styles/Home.css';
+
 const Home: React.FC = () => {
 
   const [itemsList, setItemsList] = useState<ItemEntry[]>([]);
-  const getItems = axios.get('https://gp-super-store-api.herokuapp.com/item/list?size=29');
+  const [searchString, setSearchString] = useState<string>(''); 
+  const getQuery = axios.get('https://gp-super-store-api.herokuapp.com/item/list?size=29&q=' + searchString);
 
-  useEffect( () => {
-    getItems
+  const getItems = () => {
+    getQuery
     .then( res => {
       const {items} = res.data;
       setItemsList(items);
     })
     .catch( err => console.log(err))
+  }
+
+  useEffect( () => {
+    getItems()
   }, [])  
-
-
 
   return(
     <div className='page-wrapper' id='Home'>
+      <div className='search-container'>
+        <input 
+        type='text' 
+        placeholder='Search'
+        onChange={ (evt) => {
+          setSearchString(evt.currentTarget.value)
+        } }
+        />
+        <button onClick={getItems}>O</button>
+      </div>
 
       <div className='content'>
         <ItemContainer itemsList={itemsList} />
