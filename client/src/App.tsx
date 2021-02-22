879856bd54d72dea281d06fa9e7fcaaf0159ff87
mhoobler <hoobler.michael@gmail.com>
2021-02-22 06:52:40 -0500

    react-browser-router sanity check

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
-function App() {
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Deals from './pages/Deals';
+
+import './App.css'
+
+const App: React.FC = () => {
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+      <Switch>
+
+        <Route exact path='/'>
+          <Home />
+        </Route>
+
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+
+        <Route path='/deals'>
+          <Deals />
+        </Route>
+
+      </Switch>
+
+    </Router>
   );
 }
 
---
+++ client/src/pages/Deals.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Deals: React.FC = () => {
+
+  return (
+    <div>
+      Deals
+    </div>
+  )
+}
+
+export default Deals;
