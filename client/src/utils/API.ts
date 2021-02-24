352802d6debcd41de7268a94e101912b28fea146
mhoobler <hoobler.michael@gmail.com>
2021-02-24 15:01:51 -0500

    better params

--- client/src/utils/API.ts
+++ client/src/utils/API.ts
@@ -2,14 +2,14 @@ import axios from "axios";
 
 export default {
   getItems: (str: string | undefined = "") => {
-    return axios.get(
-      "https://gp-super-store-api.herokuapp.com/item/list?size=29&q=" + str
-    );
+    return axios.get("https://gp-super-store-api.herokuapp.com/item/list", {
+      params: { q: str, size: 29 },
+    });
   },
 
   getDeals: () => {
-    return axios.get(
-      "https://gp-super-store-api.herokuapp.com/item/list?isOnSale=true"
-    );
+    return axios.get("https://gp-super-store-api.herokuapp.com/item/list", {
+      params: { isOnSale: true },
+    });
   },
 };
