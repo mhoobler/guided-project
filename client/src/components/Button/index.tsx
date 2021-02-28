11432f05c8ba0f74c51dccc93c6662290cb42e16
mhoobler <hoobler.michael@gmail.com>
2021-02-27 19:06:39 -0500

    optional Button prop

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import "./Button.css";
 
 type ButtonProps = {
-  align: "left" | "right";
+  align?: "left" | "right";
 };
 
 const Button: React.FC<ButtonProps> = ({ align, children }) => {
   return (
-    <div className={`button-container align-${align}`}>
+    <div className={`button-container ${align ? `align-${align}` : ''}`}>
       <div>{children}</div>
     </div>
   );
