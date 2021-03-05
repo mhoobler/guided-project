2c3b2a3d770cce4b7c2ce55d5e9a19fa787f1b89
mhoobler <hoobler.michael@gmail.com>
2021-03-04 20:03:31 -0500

    Searbar done

--- client/src/components/Searchbar/index.tsx
+++ client/src/components/Searchbar/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+import CloseSVG from "../../resources/close.svg";
+import SearchSVG from "../../resources/search.svg";
+
+import "./Searchbar.css";
+
 type SearchbarProps = {
   handleSearch: (str: string) => void;
 };
@@ -13,11 +18,27 @@ const Searchbar: React.FC<SearchbarProps> = ({ handleSearch }) => {
       <input
         type="text"
         placeholder="Search"
+        value={input}
         onChange={(evt) => {
           setInput(evt.currentTarget.value);
         }}
       />
-      <button onClick={() => handleSearch(input)}>O</button>
+      <div>
+        <button
+          className="clear-search"
+          onClick={() => {
+            setInput("");
+            handleSearch("");
+          }}
+        >
+          <img src={CloseSVG} alt="Clear search" />
+        </button>
+      </div>
+      <div>
+        <button className="submit-search" onClick={() => handleSearch(input)}>
+          <img src={SearchSVG} alt="Submit Search" />
+        </button>
+      </div>
     </div>
   );
 };
