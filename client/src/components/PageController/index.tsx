5ad92d281966e0189a64f0dbe293febc97603dd7
mhoobler <hoobler.michael@gmail.com>
2021-03-05 21:36:21 -0500

    pagination progress

---
+++ client/src/components/PageController/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import "./PageController.css";
+
+type PageControllerProps = {
+  handlePage: (n: number) => void;
+  page: number;
+  isLast: boolean;
+  isFirst: boolean;
+};
+
+const PageController: React.FC<PageControllerProps> = ({
+  handlePage,
+  page,
+  isFirst,
+  isLast,
+}) => {
+  return (
+    <div className="page-controller-container row">
+      {isFirst ? (
+        <div className="page-input-group row justify-center">&nbsp;</div>
+      ) : (
+        <div className="page-input-group row justify-center">
+          <button onClick={() => handlePage(-1)}>&#60;</button>
+          <button onClick={() => handlePage(0)}>First</button>
+        </div>
+      )}
+      <div className="page-input-group">{page}</div>
+      {isLast ? (
+        <div className="page-input-group row justify-center">&nbsp;</div>
+      ) : (
+        <div className="page-input-group row justify-center">
+          <button onClick={() => handlePage(1)}>&#62;</button>
+          <button onClick={() => console.log("last")}>Last</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PageController;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,13 +2,42 @@ import React, { useState } from "react";
 
 import Searchbar from "../components/Searchbar";
 import ItemContainer from "../components/ItemContainer";
+import PageController from "../components/PageController";
 
 import useGetList from "../utils/useGetList";
 
+const querySize = 18;
+const pageSize = 6;
+
 const Home: React.FC = () => {
   const [search, setSearch] = useState("");
-  const query: ListQuery = { size: 18, q: search };
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState<ListQuery>({
+    size: querySize,
+    q: search,
+    from: 0,
+  });
   const itemsList = useGetList(query);
+  console.log(itemsList);
+  const p = (page * pageSize) % querySize;
+  // console.log(p);
+  const itemsSliced = [...itemsList].slice(p, p + pageSize);
+  console.log(itemsSliced);
+
+  const handlePage = (n: number) => {
+    const newFrom = Math.floor(((page + n) * pageSize) / querySize) * querySize;
+    console.log(newFrom);
+
+    if (newFrom !== query.from) {
+      setQuery({
+        size: querySize,
+        q: search,
+        from: newFrom,
+      });
+    }
+
+    setPage(page + n);
+  };
 
   return (
     <div className="page-wrapper" id="Home">
@@ -20,14 +49,20 @@ const Home: React.FC = () => {
 
       {/* Oops message shows for a bit after clearing search, should find a way to do spinner while loading results */}
       <div className="content">
-        {itemsList.length > 0 ? (
-          <ItemContainer itemsList={itemsList} />
+        {itemsSliced.length > 0 ? (
+          <ItemContainer itemsList={itemsSliced} />
         ) : search !== "" ? (
           <h2 className="center-text">No results</h2>
         ) : (
           <h2 className="center-text">Oops, looks like something went wrong</h2>
         )}
       </div>
+      <PageController
+        handlePage={handlePage}
+        page={page}
+        isLast={itemsList.length < querySize}
+        isFirst={page < 1}
+      />
     </div>
   );
 };
