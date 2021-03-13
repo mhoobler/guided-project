059d8a26769390251743645759349cd3a8cfdd42
mhoobler <hoobler.michael@gmail.com>
2021-03-13 15:51:34 -0500

    Added button in Checkout

--- client/src/pages/Checkout.tsx
+++ client/src/pages/Checkout.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import ButtonContainer from "../components/ButtonContainer";
 
 const Checkout: React.FC = () => {
-  return <div>Thank you for shopping at Super Store!</div>;
+  return (
+    <div className="page-wrapper" id="Checkout">
+      <h1 className="center-text">Thank you for shopping at Super Store!</h1>
+      <ButtonContainer>
+        <Link to={"/home"}> Return Home </Link>
+      </ButtonContainer>
+    </div>
+  );
 };
 
 export default Checkout;
